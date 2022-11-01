6fd8bc0f41d6849c510375de5778d4925b9dbc13
Bene Martin <h043098@stud.u-szeged.hu>
2022-11-01 13:04:36 +0100

    Improve navigation

--- frontend/src/app/component/timetable-side-menu/timetable-side-menu.component.ts
+++ frontend/src/app/component/timetable-side-menu/timetable-side-menu.component.ts
@@ -1,6 +1,7 @@
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component } from '@angular/core';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { Router } from '@angular/router';
 import { SideMenuNodes } from 'src/app/shared/enum/side-menu-nodes';
 import { TimetableSideMenuNode } from 'src/app/shared/model/timetable/timetable-side-menu-node';
 
@@ -15,7 +16,7 @@ export class TimetableSideMenuComponent {
   );
   dataSource = new MatTreeNestedDataSource<TimetableSideMenuNode>();
 
-  constructor() {
+  constructor(private router: Router) {
     this.dataSource.data = SideMenuNodes.TimetableSideMenuNodes;
   }
 
@@ -23,6 +24,12 @@ export class TimetableSideMenuComponent {
     !!node.children && node.children.length > 0;
 
   onMenuElementSelected(menuElementId: number) {
-    
+    if(menuElementId === 11) this.router.navigateByUrl('timetable/timetable-weekly');
+    else if(menuElementId === 12) this.router.navigateByUrl('timetable/timetable-daily');
+    else if(menuElementId === 21) this.router.navigateByUrl('timetable/subject');
+    else if(menuElementId === 22) this.router.navigateByUrl('timetable/teacher');
+    else if(menuElementId === 23) this.router.navigateByUrl('timetable/lesson');
+    else if(menuElementId === 31) this.router.navigateByUrl('timetable/agenda-list');
+    else if(menuElementId === 32) this.router.navigateByUrl('timetable/agenda-monthly');
   }
 }
