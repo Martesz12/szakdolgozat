a82d0683117e16dbb00adafef120ff838a6b85d1
Bene Martin <h043098@stud.u-szeged.hu>
2023-02-09 12:44:38 +0100

    subject-data-operations component

--- frontend/src/app/timetable/component/subject-view/subject-data-operations/subject-data-operations.component.ts
+++ frontend/src/app/timetable/component/subject-view/subject-data-operations/subject-data-operations.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { SubjectService } from 'src/app/shared/service/timetable/subject.service';
 
 @Component({
   selector: 'app-subject-data-operations',
   templateUrl: './subject-data-operations.component.html',
   styleUrls: ['./subject-data-operations.component.scss']
 })
-export class SubjectDataOperationsComponent implements OnInit {
+export class SubjectDataOperationsComponent {
+  constructor(private subjcetService: SubjectService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  isStateTheCurrentPageState(state: string): boolean {
+      return state === this.subjcetService.getSubjectDataOperationPageState();
   }
-
 }
