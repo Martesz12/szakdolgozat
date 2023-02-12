871d1170f2d12bc8f540382ad7b1c26059108e66
Bene Martin <h043098@stud.u-szeged.hu>
2023-02-12 12:22:17 +0100

    subject description

--- frontend/src/app/timetable/component/subject-view/subject-data-operations/subject-data-operations-description/subject-data-operations-description.component.ts
+++ frontend/src/app/timetable/component/subject-view/subject-data-operations/subject-data-operations-description/subject-data-operations-description.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SubjectDto } from 'src/app/shared/model/timetable/dto/subject.dto';
+import { SubjectService } from 'src/app/shared/service/timetable/subject.service';
 
 @Component({
   selector: 'app-subject-data-operations-description',
   templateUrl: './subject-data-operations-description.component.html',
   styleUrls: ['./subject-data-operations-description.component.scss']
 })
-export class SubjectDataOperationsDescriptionComponent implements OnInit {
+export class SubjectDataOperationsDescriptionComponent {
+  selectedSubject: SubjectDto = {} as SubjectDto;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private subjectService: SubjectService) {
+      this.getSelectedSubject();
   }
 
+  private getSelectedSubject() {
+      this.subjectService.getSelectedSubjectSubject().subscribe(subject => {
+          this.selectedSubject = subject;
+      });
+  }
+
+  getScreenWidth(): number{
+      return window.innerWidth;
+  }
+
+  isInMobileView(): boolean{
+      return this.getScreenWidth() <= 599;
+  }
 }
