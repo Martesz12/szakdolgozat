9ad217124803b1426a833039498d1837f3e7c562
Bene Martin <h043098@stud.u-szeged.hu>
2023-02-12 16:51:51 +0100

    lesson navigation

--- frontend/src/app/timetable/component/lesson-view/lesson-data-operations/lesson-data-operations-navigation/lesson-data-operations-navigation.component.ts
+++ frontend/src/app/timetable/component/lesson-view/lesson-data-operations/lesson-data-operations-navigation/lesson-data-operations-navigation.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DataOperationPageState } from 'src/app/shared/enum/DataOperationPageState.enum';
+import { LessonService } from 'src/app/shared/service/timetable/lesson.service';
 
 @Component({
   selector: 'app-lesson-data-operations-navigation',
   templateUrl: './lesson-data-operations-navigation.component.html',
   styleUrls: ['./lesson-data-operations-navigation.component.scss']
 })
-export class LessonDataOperationsNavigationComponent implements OnInit {
+export class LessonDataOperationsNavigationComponent {
 
-  constructor() { }
+  constructor(private lessonService: LessonService) {}
 
-  ngOnInit(): void {
+  setPageState(state: string) {
+      if (state === DataOperationPageState.Base) this.lessonService.removeSelectedLesson();
+      this.lessonService.setLessonDataOperationPageState(state as DataOperationPageState);
+  }
+
+  isStateTheCurrentPageState(state: string): boolean {
+      return state === this.lessonService.getLessonDataOperationPageState();
   }
 
 }
