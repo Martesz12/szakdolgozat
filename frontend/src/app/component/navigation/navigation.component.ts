f457c201f0320dcaede50af6087c5b9f98c83fef
Bene Martin <h043098@stud.u-szeged.hu>
2023-02-18 16:38:24 +0100

    modify drawer style

--- frontend/src/app/component/navigation/navigation.component.ts
+++ frontend/src/app/component/navigation/navigation.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { MatDrawer, MatSidenav } from '@angular/material/sidenav';
+
+export const drawerModes = ['side', 'over'] as const;
+export type DrawerModes = typeof drawerModes[number];
 
 @Component({
     selector: 'app-navigation',
@@ -7,11 +10,33 @@ import { MatSidenav } from '@angular/material/sidenav';
     styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
-    constructor() {}
+    @ViewChild('drawer') drawer!: MatDrawer;
+    drawerMode: DrawerModes = 'side';
+    constructor(private renderer: Renderer2) {
+        this.renderer.listen('window', 'resize', this.scrollEvent);
+        this.drawerMode = this.isInMobileView() ? 'over' : 'side';
+    }
 
     ngOnInit(): void {}
 
     onToggleSidenav(sidenav: MatSidenav) {
         sidenav.toggle();
     }
+
+    getScreenWidth(): number {
+        return window.innerWidth;
+    }
+
+    isInMobileView(): boolean {
+        return this.getScreenWidth() <= 599;
+    }
+    scrollEvent = (e: Event) => {
+        if (this.isInMobileView()) {
+            if (this.drawerMode === 'side') this.drawer.toggle();
+            this.drawerMode = 'over';
+        } else {
+            if (this.drawerMode === 'over') this.drawer.toggle();
+            this.drawerMode = 'side';
+        }
+    };
 }
