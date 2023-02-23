1c6fbab9535ef99a443e946d711472289e61306a
Bene Martin <h043098@stud.u-szeged.hu>
2023-02-23 11:22:31 +0100

    task naviagtion component

--- frontend/src/app/timetable/component/agenda-list-view/agenda-list-view-data-operations/task-data-operations-navigation/task-data-operations-navigation.component.ts
+++ frontend/src/app/timetable/component/agenda-list-view/agenda-list-view-data-operations/task-data-operations-navigation/task-data-operations-navigation.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DataOperationPageState } from 'src/app/shared/enum/DataOperationPageState.enum';
+import { MainTaskService } from 'src/app/shared/service/timetable/main-task.service';
 
 @Component({
   selector: 'app-task-data-operations-navigation',
   templateUrl: './task-data-operations-navigation.component.html',
   styleUrls: ['./task-data-operations-navigation.component.scss']
 })
-export class TaskDataOperationsNavigationComponent implements OnInit {
+export class TaskDataOperationsNavigationComponent {
 
-  constructor() { }
+    constructor(private mainTaskService: MainTaskService) {}
 
-  ngOnInit(): void {
-  }
+    setPageState(state: string) {
+        if (state === DataOperationPageState.Base) this.mainTaskService.removeSelectedMainTask();
+        this.mainTaskService.setMainTaskDataOperationPageState(state as DataOperationPageState);
+    }
+  
+    isStateTheCurrentPageState(state: string): boolean {
+        return state === this.mainTaskService.getMainTaskDataOperationPageState();
+    }
 
 }
