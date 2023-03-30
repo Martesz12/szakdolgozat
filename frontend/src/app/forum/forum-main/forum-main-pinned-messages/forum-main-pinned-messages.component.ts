9e8083d27982d300045ddecbdd8b3c3a01d6ff7c
Bene Martin <h043098@stud.u-szeged.hu>
2023-03-30 15:10:54 +0200

    display files and images in pinned messages dialog

--- frontend/src/app/forum/forum-main/forum-main-pinned-messages/forum-main-pinned-messages.component.ts
+++ frontend/src/app/forum/forum-main/forum-main-pinned-messages/forum-main-pinned-messages.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MessageDto } from '../../../shared/model/forum/message.dto';
 import { UserDto } from '../../../shared/model/authentication/dto/user.dto';
+import { FileWebService } from '../../../shared/service/api/file-web.service';
 
 @Component({
     selector: 'app-forum-main-pinned-messages',
@@ -13,11 +14,23 @@ export class ForumMainPinnedMessagesComponent implements OnInit {
     userMap: Map<number, UserDto> = new Map<number, UserDto>();
     constructor(
         @Inject(MAT_DIALOG_DATA)
-        public data: { messages: MessageDto[]; userMap: Map<number, UserDto> }
+        public data: { messages: MessageDto[]; userMap: Map<number, UserDto> },
+        public fileWebService: FileWebService
     ) {
         this.userMap = data['userMap'];
         this.pinnedMessage = data['messages'].filter(message => message.pinned).reverse();
     }
 
     ngOnInit(): void {}
+
+    downloadFile(fileName: string): void {
+        this.fileWebService.getMessageFile(fileName).subscribe(file => {
+            const elem = window.document.createElement('a');
+            elem.href = window.URL.createObjectURL(file);
+            elem.download = fileName;
+            document.body.appendChild(elem);
+            elem.click();
+            document.body.removeChild(elem);
+        });
+    }
 }
